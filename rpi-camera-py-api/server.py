b2b562a77e18dc70264498344f01ca8503676fd7
Ahlrenz Arqueza <aq.arqueza@gmail.com>
2020-12-10 14:33:15 +0800

    Added capture function

--- rpi-camera-py-api/server.py
+++ rpi-camera-py-api/server.py
@@ -8,12 +8,60 @@ from shutil import copy2
 import cv2
 import numpy as np
 import os
+import time, threading
+import subprocess
+import time
 
+current_milli_time = lambda: int(round(time.time() * 1000))
 db_connect = create_engine('sqlite:///realestate-dslr.db')
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 api = Api(app)
 CORS(app)
+cameraInterval = None
+
+def capturecommand(shutterspeed, foldername):
+    dirpath = os.path.join(app.config['MEDIA_PATH'], "__temp/" + foldername)
+    try:
+        subprocess.check_call(["gphoto2", "--set-config", "shutterspeed=" + str(shutterspeed), "--capture-image-and-download"], cwd=dirpath)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+    except OSError:
+        return False
+
+def readSampleImagesAndTimes(foldername):
+    filenames = []
+    path = os.path.join(app.config['MEDIA_PATH'], foldername)
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            filenames.append(os.path.join(path, file))
+    # filenames = [
+    #            "images/memorial0061.jpg",
+    #            "images/memorial0062.jpg",
+    #            "images/memorial0063.jpg",
+    #            "images/memorial0064.jpg",
+    #            "images/memorial0065.jpg",
+    #            "images/memorial0066.jpg",
+    #            "images/memorial0067.jpg",
+    #            "images/memorial0068.jpg",
+    #            "images/memorial0069.jpg",
+    #            "images/memorial0070.jpg",
+    #            "images/memorial0071.jpg",
+    #            "images/memorial0072.jpg",
+    #            "images/memorial0073.jpg",
+    #            "images/memorial0074.jpg",
+    #            "images/memorial0075.jpg",
+    #            "images/memorial0076.jpg"
+    #            ]
+
+    images = []
+    for filename in filenames:
+        im = cv2.imread(filename)
+        images.append(im)
+
+    return images
+   
 
 class HomeProperties(Resource):
     def get(self):
@@ -48,7 +96,7 @@ class HomeRooms(Resource):
         Mediapath = request.json['mediapath']
         
         dest_filename = str(PropertyId) + "~" + Name + ".jpg";
-        dest_dir = os.path.join(app.config['MEDIA_PATH'], str(PropertyId) + "/")
+        dest_dir = os.path.join(app.config['MEDIA_PATH'], "final-blended/" + str(PropertyId) + "/")
         os.makedirs(dest_dir, exist_ok=True);
         dest_filepath = os.path.join(dest_dir, dest_filename)
         copy2(Mediapath, dest_filepath)
@@ -70,18 +118,53 @@ class Camera(Resource):
     def get(self, scene):
         # scene = request.args.get('scene')
         print("Scene: " + scene)
+
+        print("Capturing images ... ")
+
+        getFNumberString = subprocess.call('gphoto2 --get-config shutterspeed', shell=True)
+
+        getShutterChoice = subprocess.check_output(["gphoto2", "--get-config", "shutterspeed"])
+        getShutterChoice = str(getShutterChoice)
+        
+        currentIndex = getShutterChoice.index("Current: ")
+        nextLineIndex = getShutterChoice.index("\\n", currentIndex + 9)
+        currentShutterVal = getShutterChoice[(currentIndex+9):nextLineIndex]
+
+        choiceIndex = getShutterChoice[nextLineIndex:].index(currentShutterVal)
+        lastLineBreakIndex = getShutterChoice[ : (choiceIndex+nextLineIndex) ].rfind("\\n")
+        choiceNumberIndex = getShutterChoice[lastLineBreakIndex : (choiceIndex+nextLineIndex)].index("Choice: ")
+
+        choiceNumberIndex = lastLineBreakIndex + choiceNumberIndex + 8
+        choiceNumSpaceIndex = getShutterChoice[choiceNumberIndex : (choiceIndex+nextLineIndex)].index(" ")
+
+        choiceNumber = getShutterChoice[choiceNumberIndex : (choiceNumberIndex + choiceNumSpaceIndex)]
+        choiceNumber = int(choiceNumber)
+
+        captureKey = str(current_milli_time())
+
+        if scene == 'outdoor':
+            noerror = True
+            speed = choiceNumber - 6
+            while noerror == True and speed != (choiceNumber + 6):
+                noerror = capturecommand(speed, captureKey)
+                speed = speed + 6
+
+            if noerror == False:
+                return {'status': 'error', 'message': 'Camera busy.'}
+            
+        else:
+            noerror = True
+            speed = choiceNumber - 6
+            while noerror == True and speed != (choiceNumber + 6):
+                noerror = capturecommand(speed, captureKey)
+                speed = speed + 3
+
+            if noerror == False:
+                return {'status': 'error', 'message': 'Camera busy.'}
+
         print("Reading images ... ")
   
-        # if len(sys.argv) > 1:
-        #     # Read images from the command line
-        #     images = []
-        #     for filename in sys.argv[1:]:
-        #     im = cv2.imread(filename)
-        #     images.append(im)
-        #     needsAlignment = False
-        # else :
-            # Read example images
-        images = readSampleImagesAndTimes()
+        images = readSampleImagesAndTimes(captureKey)
         needsAlignment = False
         
         # Align input images
@@ -98,92 +181,47 @@ class Camera(Resource):
         exposureFusion = mergeMertens.process(images)
 
         # Save output image
-        print("Saving output ... temp.jpg")
-        os.makedirs(os.path.join(app.config['MEDIA_PATH'], "__tmp"), exist_ok=True)
-        outputpath = os.path.join(app.config['MEDIA_PATH'], "__tmp/temp.jpg")
+        print("Saving output ... " + captureKey + ".jpg")
+        os.makedirs(os.path.join(app.config['MEDIA_PATH'], "__temp/__blended"), exist_ok=True)
+        outputpath = os.path.join(app.config['MEDIA_PATH'], "__temp/__blended/" + captureKey + ".jpg")
         cv2.imwrite(outputpath, exposureFusion * 255)
         return outputpath
         
+class setInterval :
+    def __init__(self,interval,action) :
+        self.interval=interval
+        self.action=action
+        self.stopEvent=threading.Event()
+        thread=threading.Thread(target=self.__setInterval)
+        thread.start()
 
-def readSampleImagesAndTimes():
+    def __setInterval(self) :
+        nextTime=time.time()+self.interval
+        while not self.stopEvent.wait(nextTime-time.time()) :
+            nextTime+=self.interval
+            self.action()
+
+    def cancel(self) :
+        self.stopEvent.set()
+
+def capture_image():
+    print("Interval function running...")
+
+class StreamCamera(Resource):
+
+    def get(self, enabled):
+        global cameraInterval
+        # scene = request.args.get('scene')
+        print("enabled: " + enabled)
+        if enabled == "true" :
+            print("Enabling interval to capture-stream image ... ")
+            cameraInterval = setInterval(2, capture_image)
+        else:
+            print("Disabling capture-stream image ... ")
+            cameraInterval.cancel()
   
-  filenames = [
-               "images/memorial0061.jpg",
-               "images/memorial0062.jpg",
-               "images/memorial0063.jpg",
-               "images/memorial0064.jpg",
-               "images/memorial0065.jpg",
-               "images/memorial0066.jpg",
-               "images/memorial0067.jpg",
-               "images/memorial0068.jpg",
-               "images/memorial0069.jpg",
-               "images/memorial0070.jpg",
-               "images/memorial0071.jpg",
-               "images/memorial0072.jpg",
-               "images/memorial0073.jpg",
-               "images/memorial0074.jpg",
-               "images/memorial0075.jpg",
-               "images/memorial0076.jpg"
-               ]
-
-  images = []
-  for filename in filenames:
-    im = cv2.imread(filename)
-    images.append(im)
-  
-  return images
-
-
-# class Employees(Resource):
-#     def get(self):
-#         conn = db_connect.connect() # connect to database
-#         query = conn.execute("select * from employees") # This line performs query and returns json result
-#         return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
-    
-#     def post(self):
-#         conn = db_connect.connect()
-#         print(request.json)
-#         LastName = request.json['LastName']
-#         FirstName = request.json['FirstName']
-#         Title = request.json['Title']
-#         ReportsTo = request.json['ReportsTo']
-#         BirthDate = request.json['BirthDate']
-#         HireDate = request.json['HireDate']
-#         Address = request.json['Address']
-#         City = request.json['City']
-#         State = request.json['State']
-#         Country = request.json['Country']
-#         PostalCode = request.json['PostalCode']
-#         Phone = request.json['Phone']
-#         Fax = request.json['Fax']
-#         Email = request.json['Email']
-#         query = conn.execute("insert into employees values(null,'{0}','{1}','{2}','{3}', \
-#                              '{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}', \
-#                              '{13}')".format(LastName,FirstName,Title,
-#                              ReportsTo, BirthDate, HireDate, Address,
-#                              City, State, Country, PostalCode, Phone, Fax,
-#                              Email))
-#         return {'status':'success'}
-
-    
-# class Tracks(Resource):
-#     def get(self):
-#         conn = db_connect.connect()
-#         query = conn.execute("select trackid, name, composer, unitprice from tracks;")
-#         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-#         return jsonify(result)
-
-    
-# class Employees_Name(Resource):
-#     def get(self, employee_id):
-#         conn = db_connect.connect()
-#         query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
-#         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-#         return jsonify(result)
-
-class TestResource(Resource):
-    def get(self):
-        return 'hello world'
+        
+        return {'status':'success'}
 
 # api.add_resource(Employees, '/employees') # Route_1
 # api.add_resource(Tracks, '/tracks') # Route_2
@@ -192,7 +230,7 @@ api.add_resource(HomeProperties, '/homeproperties')
 api.add_resource(HomeRooms, '/homeproperties/<id>')
 api.add_resource(StaticFileServer, '/staticfile/<path:filedir>')
 api.add_resource(Camera, '/camera/<scene>')
-api.add_resource(TestResource, '/test') # Route_Test
+api.add_resource(StreamCamera, '/stream/<enabled>')
 
 if __name__ == '__main__':
-     app.run()
+    app.run()
